8725af3f553694b344155d132f64e048849fe829
rutrus <rutrus@gmail.com>
2015-06-09 13:11:51 +0200

    broadcast plugin fixed and ready for pull request

--- plugins/broadcast.lua
+++ plugins/broadcast.lua
@@ -1,74 +1,101 @@
-local clock = os.clock
+MSGS = 5
+STOP = 1.0
 
-function sleep(n)  -- seconds
-  local t0 = clock()
-  while clock() - t0 <= n do end
+local function is_channel_disabled( receiver )
+    if not _config.disabled_channels then
+        return false
+    end
+    if _config.disabled_channels[receiver] == nil then
+        return false
+    end
+  return _config.disabled_channels[receiver]
 end
 
-local function async(msg)
-   send_msg(msg.receiver, msg.text, ok_cb, false)
-   --~ print("send_msg("..msg.receiver..", "..msg.text..", ok_cb, false)")
+local function async(params)
+    local i = 0
+    while i<MSGS do
+        n = #params.receivers
+        if n ~= 0 then
+            if not is_channel_disabled(params.receivers[n]) then
+                -- Comment next line for debug:
+                send_large_msg(params.receivers[n],params.text)
+                print(n.."Message sended to "..params.receivers[n])
+            else print(n.."Channel disabled: "..params.receivers[n])
+            end
+            table.remove(params.receivers, n)
+            i = i+1
+        else break
+        end
+    end
+    print('stop '..STOP..' second(s)')
+    if #params.receivers ~= 0 then
+        return postpone(async,{receivers=params.receivers,text=params.text},STOP)
+    else return
+    end
 end
 
 local function send_generic(keybase, keymatch, receiverbase , text)
-   local keys = redis:keys(keybase)
-   local already = {}
-   if n == nil then
-      n = 0
-   end
-   for k,key in pairs(keys) do
-      if n > 990 then
-         return n
-      end
-      local id = tonumber(string.match(key, keymatch))
-      local receiver = receiverbase .. id
-      if not already[id] and id ~= our_id then
-         n = n + 1
-         -- print("\t" .. receiver)
-         local msg = {receiver=receiver, text=text}
-         postpone (async(msg),false, 1)
-         --~ if n % 5 == 0 then sleep(1) end
-         -- send_msg(receiver, text, ok_cb, false)
-         already[id] = true
-      end
-   end
-   return n
+    local keys = redis:keys(keybase)
+    local already = {}
+    local receivers = {}
+    if n == nil then
+        n = 0
+    end
+    for k,key in pairs(keys) do
+        local id = tonumber(string.match(key, keymatch))
+        table.insert(receivers, receiverbase .. id)
+    end
+    return receivers
 end
 
 local function send_chats(text)
-   print("Sending broadcast to chats")
-   return send_generic("chat:*:users", "chat:(%d+).*", "chat#id", text, n)
+    print("Sending broadcast to chats")
+    return send_generic("chat:*:users", "chat:(%d+).*", "chat#id", text, n)
 end
 
 local function send_users(text)
-   print("Sending broadcast to users")
-   return send_generic("msgs:*:" .. our_id, "msgs:(%d+).*", "user#id", text, n)
+    print("Sending broadcast to users")
+    return send_generic("msgs:*:" .. our_id, "msgs:(%d+).*", "user#id", text, n)
 end
 
--- local function get_n()
---    local n1 = redis:keys("chat:*:users")
---    local n2 = redis:keys("msgs:*:" .. our_id)
---    return #n1, #n2
--- end
-
 local function run(msg, matches)
-   local text = matches[1]
-   local nchats = send_chats(text)
-   local nusers = send_users(text, nchats)
-   -- postpone (send_chats, text, 1)
-   -- postpone (send_users, text, 1)
-   -- nchats, nusers = get_n()
-   return "Your message had been sended to " .. nchats .. " chats and " .. nusers .. " users."
+    local text = ""
+    local receivers = {}
+    local chats = {}
+    local nusers = 0
+    local nchats = 0
+
+    if #matches == 1 then
+        text = matches[1]
+        receivers = send_users(text)
+    elseif #matches == 2 then
+        text = matches[2]
+    end
+
+    chats = send_chats(text)
+    nusers = #receivers
+    nchats = #chats
+
+print(#matches)
+    for i=1,#chats do
+        receivers[#receivers+1] = chats[i]
+    end
+    --~ vardump(receivers)
+    print( "Your message was sended to " .. nchats .. " chats and " .. nusers .. " users.")
+    async({receivers=receivers,text=text})
+    return "Your message was sended to " .. nchats .. " chats and " .. nusers .. " users."
 end
 
 return {
-   description = "Send a broadcast message to all chats and users.",
-   usage = {
-      ".!broadcast (message): Send the message to all chats and users."
-   },
-   patterns = {
-      "^!broadcast (.*)$"
-   },
-   privileged = true,
-   run = run
+    description = "Send a broadcast message to all chats and users.",
+    usage = {
+          "!broadcast (message): Send the message to all chats and users."
+        .."!broadcast (groups) (message): Send the message to group chats"
+    },
+    patterns = {
+        "^!broadcast (groups?) (.*)",
+        "^!broadcast (.*)"
+    },
+    privileged = true,
+    run = run
 }
